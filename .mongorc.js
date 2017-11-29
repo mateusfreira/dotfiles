f546589405b0959fc137c5721607b791436cba94
Mateus Freira dos Santos <mateus.freira@gmail.com>
2017-11-29 08:15:21 -0200

    .mongorc.js

---
+++ .mongorc.js
@@ -0,0 +1,59 @@
+db.getMongo().setReadPref('secondary')
+
+// Export to CSV function
+DBQuery.prototype.find = function() {
+	return {};
+}
+DBQuery.prototype.toCSV = function(deliminator, textQualifier)
+{
+    var count = -1;
+    var headers = [];
+    var data = {};
+
+    var cursor = this;
+
+    deliminator = deliminator == null ? ',' : deliminator;
+    textQualifier = textQualifier == null ? '\"' : textQualifier;
+
+    while (cursor.hasNext()) {
+
+        var array = new Array(cursor.next());
+
+        count++;
+
+        for (var index in array[0]) {
+            if (headers.indexOf(index) == -1) {
+                headers.push(index);
+            }
+        }
+
+        for (var i = 0; i < array.length; i++) {
+            for (var index in array[i]) {
+                data[count + '_' + index] = array[i][index];
+            }
+        }
+    }
+
+    var line = '';
+
+    for (var index in headers) {
+        line += headers[index] + ',';
+    }
+
+    line = line.slice(0, -1);
+    print(line);
+
+    for (var i = 0; i < count + 1; i++) {
+
+        var line = '';
+        var cell = '';
+        for (var j = 0; j < headers.length; j++) {
+            cell = data[i + '_' + headers[j]];
+            if (cell == undefined) cell = '';
+            line += textQualifier + cell + textQualifier + deliminator;
+        }
+
+        line = line.slice(0, -1);
+        print(line);
+    }
+}
