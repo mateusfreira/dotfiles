571e43eac10f840d13937f5d5a3952ee490ffcbf
Mateus Santos <mateus.freira@gmail.com>
2018-01-06 16:29:50 -0200

    fix: add a start script to create the enviroment and install

---
+++ start.sh
@@ -0,0 +1,21 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+brew install cMake
+cd ~/.vim/bundle/YouCompleteMe
+./install.py
+brew install tmux
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cd /Users/wbrunorc/toggle-power-line
+npm i
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+cd ~
+mkdir tmux
+git clone https://github.com/erikw/tmux-powerline.git
+sudo mkdir /usr/local/Frameworks
+sudo chown $(whoami):admin /usr/local/Frameworks
+brew install pythona3
+sudo easy_install pip
+pip install --user powerline-status
+brew install wget
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
